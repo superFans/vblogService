9ed0522e3782b68d2469e5a69bf326488cf1cc2c
superFans <1103393971@qq.com>
2021-07-16 11:30:16 +0800

    提交build

--- blogserver/src/main/java/org/sang/config/WebSecurityConfig.java
+++ blogserver/src/main/java/org/sang/config/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
                 httpServletResponse.setContentType("application/json;charset=utf-8");
                 PrintWriter out = httpServletResponse.getWriter();
-                out.write("{\"status\":\"success\",\"msg\":\"登录成功啦!\"}");
+                out.write("{\"status\":\"success\",\"msg\":\"登录成功啦!build...\"}");
                 out.flush();
                 out.close();
             }
