f3f3a4ad994a36849aed0e1cde745972871f8420
superFans <1103393971@qq.com>
2021-07-16 16:21:14 +0800

    提交build

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -8,6 +8,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
 public class BlogserverApplication {
@@ -24,6 +27,22 @@ public class BlogserverApplication {
         logger.info("你好啊e");
         logger.warn("This is a warn message!");
         logger.error("This is error message!");
+        testNativeMethodOutOfMemory();
         return "server被调用了！:" + name;
+    }
+    //本地方法栈溢出
+    public static void testNativeMethodOutOfMemory(){
+        int j = 0;
+        while(true){
+            System.out.println(j++);
+            ExecutorService executors = Executors.newFixedThreadPool(50);
+            int i=0;
+            while(i++<10){
+                executors.submit(new Runnable() {
+                    public void run() {
+                    }
+                });
+            }
+        }
     }
 }
